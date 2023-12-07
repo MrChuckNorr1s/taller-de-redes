a1d6936e40875425bd57729489a6906825a68864
MrChuckNorr1s <115842067+MrChuckNorr1s@users.noreply.github.com>
2023-12-07 13:31:09 -0300

    Update fuzz_script.py

--- scapy/fuzz_script.py
+++ scapy/fuzz_script.py
@@ -1,40 +1,19 @@
-#codigo para ambas inyecciones con Fuzzing
-
 from scapy.all import *
-from scapy.utils import hexdump
-import random
-import string
 
-def fuzz_tcp_raw_fuzzing(packet):
-    if IP in packet and TCP in packet and Raw in packet:
-        # Supongamos que el tr  fico IRC est   codificado en UTF-8
-        irc_message = packet[Raw].load.decode('utf-8')
+def fuzzing(packet):
+    modified_packet=Ether(packet[Ether]) / IP(dst=packet[IP].dst,src=packet[IP].src)
+    print("Modificando paquete")
+    # PAQUETE MODIFICAD
+    modified_packet=fuzz(modified_packet)
+    print("Paquete modificado") 
+    ls(modified_packet)
+    # ENVIAMOS EL PAQUETE
+    send(modified_packet, verbose=False)
 
-        # Crear un paquete Scapy a partir de la cadena IRC
-        scapy_packet = IP(src=packet[IP].src, dst=packet[IP].dst) / TCP(sport=packet[TCP].sport, dport=packet[TCP].dport) / Raw(load=irc_message)
+def packet_callback(packet):
+    print("Paquete Capturado")
+    print(packet.summary())
 
-        # Impresi  n del paquete original
-        print("\nOriginal Packet:")
-        hexdump(packet)
+    fuzzing(packet)
 
-        # Inyecci  n 1: Fuzzing en el mensaje IRC
-        fuzzed_packet = fuzz(scapy_packet)
-        send(fuzzed_packet)
-
-        # Impresi  n del paquete despu  s de la inyecci  n 1
-        print("\nFuzzed Packet(n°1):")
-        hexdump(fuzzed_packet)
-
-        # Inyecci  n 2: T  cnica de fuzzing tradicional en el campo Raw
-        fuzzed_message = ''.join(random.choice(string.ascii_letters) for _ in range(len(irc_message)))
-        fuzzed_packet = packet.copy()
-        fuzzed_packet[Raw].load = fuzzed_message.encode('utf-8')
-        send(fuzzed_packet)
-
-        # Impresi  n del paquete despu  s de la inyecci  n 2
-        print("\nFuzzed Packet (Traditional Fuzzing, n°2):")
-        hexdump(fuzzed_packet)
-
-# Filtra el tr  fico entre el servidor (172.17.0.2) y el cliente (172.17.0.3) IRC
-filter_expression = "host 172.17.0.2 and host 172.17.0.3 and port 6667"
-sniff(filter=filter_expression, prn=fuzz_tcp_raw_fuzzing, store=0)
+sniff(iface='eth0',filter="tcp and host {} and port {}".format('172.17.0.2', 6667),prn=packet_callback)
